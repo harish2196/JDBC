15b0478d94b46ffb39459af8eb5cfd20196166e2
harish2196 <harishdhanush2196@gmail.com>
2023-10-30 10:13:34 +0530

    first commit

---
+++ example/Constants.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Constants {
+
+    // JDBC database URL, username, and password of MySQL server
+    public static String url = "jdbc:mysql://localhost:3306";
+    public static String user = "root";
+    public static String password = "";
+    // JDBC database - select students from table with id
+
+}
